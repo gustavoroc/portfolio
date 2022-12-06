7d2bdcf0f3a61dfaa1ddfaf9806aeb423aba89d3
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-12-06 12:26:55 -0300

    feat/chore: Ion-icon de checked p/ feedback ao usuario.

--- actions/utils/setFeedBackMessage.js
+++ actions/utils/setFeedBackMessage.js
@@ -1,7 +1,10 @@
 import toggle from "./toggle.js";
 
 const setFeedBackMessage = (message) => {
-  toggle(".feedback-container").innerHTML = `<span>${message}</span>`;
+  toggle(".feedback-container").innerHTML = `
+    <span>${message}</span>
+    <ion-icon name="checkmark-circle"></ion-icon>
+  `;
   setTimeout(() => {
     toggle(".feedback-container");
   }, 2500);
